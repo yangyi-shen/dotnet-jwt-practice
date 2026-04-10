cc8f23564f34bb9c78d90feaecb543cca145d48e
Yang-Yi Shen <mrshenyangyi@gmail.com>
2026-04-10 15:59:22 +0700

    feat: tạo class dto  yêu cầu login

---
+++ DotnetPractice/Models/Requests/LoginRequest.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetPractice.Models.Requests
+{
+    public class LoginRequest
+    {
+        public required string Password { get; set; }
+    }
+}
