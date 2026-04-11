1c8374b8758fde18642ee7b490f65d52bcb04c05
Yang-Yi Shen <mrshenyangyi@gmail.com>
2026-04-11 11:04:21 +0700

    feat: tạo hàm service đăng ký và đăng nhập

--- DotnetPractice/Services/UserService.cs
+++ DotnetPractice/Services/UserService.cs
@@ -8,11 +8,13 @@ using DotnetPractice.Models.DTOs;
 using DotnetPractice.Models.Requests;
 using DotnetPractice.Models.Responses;
 using DotnetPractice.Repository;
+using DotnetPractice.Utils;
 
 namespace DotnetPractice.Services
 {
     public class UserService
     {
+        private readonly UserUtils _utils = new();
         private readonly UserRepository _repository;
 
         public UserService(UserRepository repository)
@@ -20,9 +22,38 @@ namespace DotnetPractice.Services
             _repository = repository;
         }
 
+        public async Task<ApiResponse<UserDataDTO>> RegisterUser(RegisterRequest request)
+        {
+            string userName = request.UserName;
+
+            if (!_utils.ValidateUserName(userName))
+            {
+                throw new ApiException(ApiExceptions.USER_INVALID);
+            }
+
+            User? existingUser = await _repository.GetUserByUserName(userName);
+            if (existingUser != null)
+            {
+                throw new ApiException(ApiExceptions.USER_ALREADY_EXISTS);
+            }
+
+            User newUser = new(userName);
+            await _repository.AddUser(newUser);
+
+            UserDataDTO responseContent = new(newUser);
+            ApiResponse<UserDataDTO> response = new(true, responseContent);
+
+            return response;
+        }
+
         public async Task<ApiResponse<UserDataDTO>> LoginUser(LoginRequest request)
         {
             string userName = request.UserName;
+
+            if (!_utils.ValidateUserName(userName))
+            {
+                throw new ApiException(ApiExceptions.USER_INVALID);
+            }
 
             User? user = await _repository.GetUserByUserName(userName);
             if (user == null)
