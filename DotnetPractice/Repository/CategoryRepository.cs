4fd959357f0057b70f949c22ae27116c02b23f31
Yang-Yi Shen <mrshenyangyi@gmail.com>
2026-04-11 11:54:37 +0700

    feat: tạo hàm crud cho category repository

--- DotnetPractice/Repository/CategoryRepository.cs
+++ DotnetPractice/Repository/CategoryRepository.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotnetPractice.Models.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DotnetPractice.Repository
 {
@@ -12,6 +14,35 @@ namespace DotnetPractice.Repository
         public CategoryRepository(ContentDBContext dbContext)
         {
             _dbo = dbContext;
+        }
+
+        public async Task AddCategory(Category data)
+        {
+            _dbo.Categories.Add(data);
+            await _dbo.SaveChangesAsync();
+        }
+
+        public async Task<List<Category>> GetAllCategories()
+        {
+            return await _dbo.Categories.ToListAsync();
+        }
+
+        public async Task<Category?> GetCategoryByGUID(Guid GUID)
+        {
+            return await _dbo.Categories.Where(c => c.GUID == GUID).FirstOrDefaultAsync();
+        }
+
+        public async Task<Category?> GetCategoryByCategoryName(string categoryName)
+        {
+            return await _dbo
+                .Categories.Where(c => c.CategoryName == categoryName)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task EditCategory(Category data)
+        {
+            _dbo.Categories.Update(data);
+            await _dbo.SaveChangesAsync();
         }
     }
 }
