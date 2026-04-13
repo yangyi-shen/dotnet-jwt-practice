cdcd051404752fe7d0857ded9991dc8cf18ee7a5
Yang-Yi Shen <mrshenyangyi@gmail.com>
2026-04-13 08:45:16 +0700

    feat: tạo chế hàm cho phục dụ category

--- DotnetPractice/Services/CategoryService.cs
+++ DotnetPractice/Services/CategoryService.cs
@@ -2,13 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotnetPractice.Exceptions;
+using DotnetPractice.Models.DTOs;
 using DotnetPractice.Models.Entities;
+using DotnetPractice.Models.Requests;
 using DotnetPractice.Repository;
+using DotnetPractice.Utils;
 
 namespace DotnetPractice.Services
 {
     public class CategoryService
     {
+        private readonly CategoryUtils _utils = new();
         private readonly CategoryRepository _repository;
 
         public CategoryService(CategoryRepository repository)
@@ -16,14 +21,35 @@ namespace DotnetPractice.Services
             _repository = repository;
         }
 
-        public async Task<List<Category>> GetAllCategories()
+        public async Task<ApiResponse<List<Category>>> GetAllCategories()
         {
-            return await _repository.GetAllCategories();
+            List<Category> data = await _repository.GetAllCategories();
+
+            ApiResponse<List<Category>> response = new(true, data);
+
+            return response;
         }
 
-        public async Task AddCategory(Category data)
+        public async Task<ApiResponse<Category>> AddCategory(AddCategoryRequest request)
         {
+            string categoryName = request.CategoryName;
+            if (!_utils.validateCategoryName(categoryName))
+            {
+                throw new ApiException(ApiExceptions.CATEGORY_INVALID);
+            }
+
+            Category? existingCategory = await _repository.GetCategoryByCategoryName(categoryName);
+            if (existingCategory != null)
+            {
+                throw new ApiException(ApiExceptions.CATEGORY_ALREADY_EXISTS);
+            }
+
+            Category data = new(categoryName);
             await _repository.AddCategory(data);
+
+            ApiResponse<Category> response = new(true, data);
+
+            return response;
         }
     }
 }
