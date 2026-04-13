615c565b9c793af1f5dda1b14fd37f301e0d52e0
Yang-Yi Shen <mrshenyangyi@gmail.com>
2026-04-13 10:05:48 +0700

    feat: tạo dụng cụ tiện ích so chức năng post

---
+++ DotnetPractice/Utils/PostUtils.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetPractice.Utils
+{
+    public class PostUtils
+    {
+        public bool validatePostText(string postText)
+        {
+            if (string.IsNullOrEmpty(postText))
+                return false;
+            if (postText.Length > 500)
+                return false;
+            return true;
+        }
+    }
+}
